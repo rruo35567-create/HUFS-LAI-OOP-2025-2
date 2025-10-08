4a7a4167988f6f1f700a1c3a417dc431703a5c71
Jonghwa Kim <rruo35567@gmail.com>
2025-10-08 22:30:35 +0900

    modified:   submissions/202500530/assignment2/problem4/dsops/__init__.py

--- submissions/202500530/assignment2/problem4/dsops/__init__.py
+++ submissions/202500530/assignment2/problem4/dsops/__init__.py
@@ -1,3 +1,3 @@
 from .split.train_test import train_test_split
-from .split.stats.label import label_distribution
+from .stats.label import label_distribution
 __all__ = ["train_test_split", "label_distribution"]
