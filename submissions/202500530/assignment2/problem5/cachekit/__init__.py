28634fb9cac241a3c23cb3b425602c2c6b502407
jhwa21 <rruo35567@gmail.com>
2025-10-10 23:35:14 +0900

    Update __init__.py

--- submissions/202500530/assignment2/problem5/cachekit/__init__.py
+++ submissions/202500530/assignment2/problem5/cachekit/__init__.py
@@ -5,6 +5,7 @@ def print_version_info() -> None: #(간단 출력)
 
 #gpt
 # dict의 method에 무엇이 있는지 물어보고 그에 대한 개념을 물어보았습니다.
+# 함수의 변수를 줄이는 과정과 @의 오용을 없애는 과정에서 gpt의 도움을 받았습니다.
 
 class Cache:
     def __init__(self) -> None: #내부 dict 초기화
