d0d26fe206e70aab3d4d480902dc3a4206646e68
jhwa21 <rruo35567@gmail.com>
2025-10-10 16:05:24 +0900

    Update __init__.py
    VERSION 출력 수정, @property, setter 삭제, 재귀코드 수정하였습니다

--- submissions/202500530/assignment2/problem5/cachekit/__init__.py
+++ submissions/202500530/assignment2/problem5/cachekit/__init__.py
@@ -1,7 +1,7 @@
-VERSION: "3.0"
+VERSION = "3.0"
 
 def print_version_info() -> None: #(간단 출력)
-    print(f"{VERSION}")
+    print(VERSION)
 
 #gpt
 # dict의 method에 무엇이 있는지 물어보고 그에 대한 개념을 물어보았습니다.
@@ -10,25 +10,20 @@ class Cache:
     def __init__(self) -> None: #내부 dict 초기화
         self._dict = {}
     
-    @property
-    def _dict(self) -> dict:
-        return self._dict
-    
-    @_dict.setter
     def put(self, key, value) -> None:
-        self._dict[key] = value
-        return self._dict
+        di = self._dict[key] = value
+        return di
 
     def get(self, key, default=None):
-        self._dict.get(key, default = "None")
-        return self._dict
+        ge = self._dict.get(key, default = "None")
+        return ge
 
     def __len__(self) -> int:
         le = len(self._dict)
         return le
 
     def clear(self) -> None:
-        self._dict.clear
-        return self._dict
+        cl = self._dict.clear
+        return cl
 
 __all__ = ["Cache", "print_version_info", "VERSION"]
