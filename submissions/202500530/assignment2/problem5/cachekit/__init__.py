722fd068ae9c72a32f90732f9ec6e3a0af33bf04
jhwa21 <rruo35567@gmail.com>
2025-10-10 18:42:27 +0900

    Update __init__.py

--- submissions/202500530/assignment2/problem5/cachekit/__init__.py
+++ submissions/202500530/assignment2/problem5/cachekit/__init__.py
@@ -26,26 +26,4 @@ class Cache:
         cl = self._dict.clear
         return cl
 
-''' 오류 부분을 확실하게 모르겠어서 gpt에 돌려본 결과에서 코드는 
-VERSION = "3.0"
-def print_version_info() -> None:
-    print(f"{VERSION}")
-
-class Cache:
-    def __init__(self) -> None:
-        self._dict = {}
-
-    def put(self, key, value) -> None:
-        self._dict[key] = value
-
-    def get(self, key, default=None):
-        return self._dict.get(key, default)
-
-    def __len__(self) -> int:
-        return len(self._dict)
-
-    def clear(self) -> None:
-        self._dict.clear()
-이 나왔고, 변수의 수를 줄이는 것이 더 효율적이라서 채용할까 생각했지만 일단 저의 스타일로 코딩을 남기고 싶어 각주로 코드를 남기고 제 스타일 코드를 남겨놓겠습니다. '''
-
 __all__ = ["Cache", "print_version_info", "VERSION"]
